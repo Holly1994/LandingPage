e301b78a8d98329cdbe63419cb64a9b277a75833
Holly1994 <Stefano_Ollano@yahoo.it>
2017-04-07 09:04:22 +0100

    Update stupidTest.php

--- test/stupidTest.php
+++ test/stupidTest.php
@@ -6,4 +6,11 @@
     $foo=true;
     $this ->assertTime($foo);
     }
+    public function testHTML()
+    {
+      $htmlOutput= '<p id="my_id"> ciao </p>';
+      $matcher= array ('id'=>'my_id');
+      $this->assetTag($matcher, $htmlOutput)
+      
+      
   }
